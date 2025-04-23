88551aae9e5db787adebdb381f8dc12d6ade8282
hsaun <yx530161@gmail.com>
2025-04-23 14:58:49 +0800

    2.58

--- t.py
+++ t.py
@@ -1,4 +1,5 @@
-import firebase_admin
+import firebase_admin, flask
+from flask import Flask, render_template
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -6,3 +7,16 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 db.collection("123").add({"1":2})
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("1.html")
+
+@app.route("/1")
+def indexx():
+    return render_template("2.html")
+
+if __name__ == "__main__":
+    app.run()
