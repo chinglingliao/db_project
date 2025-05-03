bcce7d43ab023fd96dd67b3ba5c1c1247d8a902d
歐佳俞 <ouchiayu@oujiayudeMacBook-Air.local>
2025-05-03 18:34:57 +0800

    修改主頁跟註冊頁面

--- t.py
+++ t.py
@@ -1,5 +1,5 @@
-import firebase_admin, flask
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
+import firebase_admin
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -10,13 +10,41 @@ db.collection("123").add({"1":2})
 
 app = Flask(__name__)
 
+#主頁
 @app.route("/")
 def index():
-    return render_template("1.html")
+    return render_template("1.html") 
 
-@app.route("/1")
-def indexx():
+#註冊頁面
+@app.route("/showsingup")
+def show_signup():
     return render_template("2.html")
+
+#註冊功能
+@app.route("/signup", methods=["POST"])
+def signup():
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+
+    # 儲存到 Firebase Firestore
+    db.collection("users").add({
+        "name": name,
+        "email": email,
+        "password": password
+    })
+
+    return "註冊成功！收到名字：" + name
+
+#登入頁面
+@app.route("/login")
+def show_login():
+    return render_template("3.html")
+
+#配對邀請頁面
+@app.route("/love")
+def show_love():
+    return render_template("4.html")
 
 if __name__ == "__main__":
     app.run()
